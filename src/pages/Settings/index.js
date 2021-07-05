f94a49a2ae36737557ed7172d703402ab337c262
moizez <moizezhenrique@gmail.com>
2021-07-05 19:29:57 -0300

    ajustes e correções de bugs

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -84,7 +84,7 @@ const Settings = () => {
 
             <View style={styles.versionContainer}>
                 <Text>Producer Point</Text>
-                <Text style={{ color: '#adb5bd', marginBottom: 12 }}>Beta v2021.05.12</Text>
+                <Text style={{ color: '#adb5bd', marginBottom: 12 }}>Producer Point v2021.07.05</Text>
             </View>
         </Container >
     )
